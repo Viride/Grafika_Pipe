5d753780e19fe387b8827647867a00a4625a85e9
Viride <pawel.kaczmarek@ingonyama.pl>
2016-08-16 22:48:21 +0200

    obsluga klawiszy, obracanie planszy

--- Grafika1/Grafika1/Main.cpp
+++ Grafika1/Grafika1/Main.cpp
@@ -20,18 +20,21 @@
 #include "myCube.h"
 // wskaŸnik na funkcjê glWindowPos2i
 
-float speed = 0; // [radiany/s]
+float speedx = 0; // [radiany/s]
+float speedy = 0;
 float observerx = 0.0;
 float observery = 0.0;
 float observerz = -40.0;
 float height = 768;
 float width = 1366;
+int numer_pola=40;
 
 struct Pola {
 	int number;				//numer elementu 0-80
 	float x_left, x_right;	// wspó³rzêdne do kwadratu
 	float y_up, y_down;		// wspó³rzêdne do kwadratu
 	int pozycja;			// u³o¿enie elementu 1-4 (tak jak jest u³o¿ony)
+	int model;				// który element ma tam staæ
 };
 Pola tab[81];
 
@@ -71,10 +74,31 @@ void key_callback(GLFWwindow* window, int key,
 		if (key == GLFW_KEY_DOWN) observery += 1.0;
 		if (key == GLFW_KEY_LEFT_BRACKET) observerz -= 1.0;
 		if (key == GLFW_KEY_RIGHT_BRACKET) observerz += 1.0;
+		if (key == GLFW_KEY_K) speedx = -3.14;
+		if (key == GLFW_KEY_I) speedx = 3.14;
+		if (key == GLFW_KEY_J) speedy = -3.14;
+		if (key == GLFW_KEY_L) speedy = 3.14;
+		if (key == GLFW_KEY_W) {
+			if (numer_pola > 8) numer_pola -= 9;
+			else numer_pola = numer_pola + 81 - 9;
+		}
+		if (key == GLFW_KEY_S) {
+			if (numer_pola < 72) numer_pola += 9;
+			else numer_pola = numer_pola + 9 - 81;
+		}
+		if (key == GLFW_KEY_D) {
+			if (numer_pola % 9 != 0) numer_pola -= 1;
+			else numer_pola = numer_pola + 8;
+		}
+		if (key == GLFW_KEY_A) {
+			if (numer_pola % 9 != 8) numer_pola += 1;
+			else numer_pola = numer_pola - 8;
+		}
 	}
 
 	if (action == GLFW_RELEASE) {
-		speed = 0;
+		speedy = 0;
+		speedx = 0;
 	}
 }
 
@@ -97,7 +121,7 @@ void initOpenGLProgram(GLFWwindow* window) {
 }
 
 //Procedura rysuj¹ca zawartoœæ sceny
-void drawScene(GLFWwindow* window, float angle) {
+void drawScene(GLFWwindow* window, float angle1, float angle2) {
 	//************Tutaj umieszczaj kod rysuj¹cy obraz******************l
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Wykonaj czyszczenie bufora kolorów
@@ -119,7 +143,8 @@ void drawScene(GLFWwindow* window, float angle) {
 
 	//Wylicz macierz obrotu o k¹t angle wokó³ osi (0,1,1)
 	glm::mat4 M = glm::mat4(1.0f);
-	M = glm::rotate(M, angle, glm::vec3(0, 1, 1));
+	M = glm::rotate(M, angle1, glm::vec3(1, 0, 0));
+	M = glm::rotate(M, angle2, glm::vec3(0, 1, 0));
 	glLoadMatrixf(glm::value_ptr(V*M)); //Za³aduj wyliczon¹ macierz do OpenGL
 
 
@@ -131,13 +156,18 @@ void drawScene(GLFWwindow* window, float angle) {
 	//glColorPointer(3, GL_FLOAT, 0, myCubeColors); //Wska¿ tablicê kolorów
 
 	//glDrawArrays(GL_QUADS, 0, myCubeVertexCount); //Wykonaj rysowanie
-	glColor3f(1.0, 0, 0);
-	glRectf(1.0, 1.0, -1.0, -1.0);
+	
 	for (int i = 0;i < 81;i++) {
-		if(i%2==0) glColor3f(1.0, 0, 0);
-		else glColor3f(0, 1.0, 0);
-		glRectf(tab[i].x_left, tab[i].y_up, tab[i].x_right, tab[i].y_down);
+		if (i == numer_pola) continue;
+		else {
+			if (i % 2 == 0) glColor3f(1.0, 0, 0);
+			else glColor3f(0, 1.0, 0);
+			glRectf(tab[i].x_left, tab[i].y_up, tab[i].x_right, tab[i].y_down);
+		}
 	}
+	
+	glColor3f(0, 0, 1.0);
+	glRectf(tab[numer_pola].x_left, tab[numer_pola].y_up, tab[numer_pola].x_right, tab[numer_pola].y_down);
 
 	glDisableClientState(GL_VERTEX_ARRAY); //Wy³¹cz u¿ywanie tablicy wierzcho³ków
 	glDisableClientState(GL_COLOR_ARRAY); //Wy³¹cz u¿ywanie tablicy kolorów
@@ -179,14 +209,18 @@ int main(void)
 
 	initOpenGLProgram(window); //Operacje inicjuj¹ce
 
-	float angle = 0; //K¹t obrotu torusa
+	float angle1 = 0;
+	float angle2 = 0;
 	glfwSetTime(0); //Wyzeruj licznik czasu
 	//glutReshapeFunc(Reshape);
 
 					//G³ówna pêtla
 	while (!glfwWindowShouldClose(window)) //Tak d³ugo jak okno nie powinno zostaæ zamkniête
 	{
-		drawScene(window, angle); //Wykonaj procedurê rysuj¹c¹
+		angle1 += speedx*glfwGetTime(); //Zwiêksz k¹t o prêdkoœæ k¹tow¹ razy czas jaki up³yn¹³ od poprzedniej klatki
+		angle2 += speedy*glfwGetTime();
+		glfwSetTime(0); //Wyzeruj licznik czasu
+		drawScene(window, angle1, angle2); //Wykonaj procedurê rysuj¹c¹
 		glfwPollEvents(); //Wykonaj procedury callback w zaleznoœci od zdarzeñ jakie zasz³y.
 	}
 
